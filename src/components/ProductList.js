51dd1c70f6a90f4b6b42abd4d095c18e4fb99ca3
ShoaibKhanJadoon <shoaibkhan332002@gmail.com>
2024-12-08 21:00:50 +0500

    product list updated

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -29,29 +29,27 @@ const ProductList = ({ filters }) => {
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
 
-          <Link href={`/Products/Product/${product[0][0]}`}>
-
-            <div className="group relative">
-              <img
-                alt={product[0][3]}
-                src={product[0][2]}
-                className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
-              />
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-
-                    <span aria-hidden="true" className="absolute inset-0" />
-                    {product[0][1]}
-
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">{product[0][5]}</p>
+        {filteredProducts.map((product) => (
+            <Link key={product.id} href={`/Products/Product/${product.id}`}>
+              <div className="group relative">
+                <img
+                  alt={product.imageAlt}
+                  src={product.imageSrc}
+                  className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
+                />
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm text-gray-700">
+                      <span aria-hidden="true" className="absolute inset-0" />
+                      {product.name}
+                    </h3>
+                    <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                  </div>
+                  <p className="text-sm font-medium text-gray-900">{product.price}</p>
                 </div>
-                <p className="text-sm font-medium text-gray-900">{product[0][4]}</p>
               </div>
-            </div>
-
-          </Link>
+            </Link>
+          ))}
 
         </div>
       </div>
